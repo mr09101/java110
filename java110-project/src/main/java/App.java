bc7666019a9be5fc192dc385acac9f3154ab1683
kim <ilos1212@naver.com>
2018-09-07 12:28:10 +0900

    Ioc 컨테이너 작성1

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+import bitcamp.java110.cms.context.ApplicationContext;
 import bitcamp.java110.cms.control.Controller;
 import bitcamp.java110.cms.control.ManagerController;
 import bitcamp.java110.cms.control.StudentController;
@@ -15,15 +16,11 @@ public class App {
     
     static Scanner keyIn = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         HashMap<String,Controller> requestHandlerMapping = new HashMap<>();
         
-        requestHandlerMapping.put("1",
-                new StudentController(new LinkedList<Student>()));
-        requestHandlerMapping.put("2",
-                new TeacherController(new ArrayList<Teacher>()));
-        requestHandlerMapping.put("3",
-                new ManagerController(new ArrayList<Manager>()));
+        ApplicationContext iocContainer = 
+                new ApplicationContext("bitcamp.java110.cms.control");
         
         while (true) {
             String menu = promptMenu();
@@ -31,7 +28,7 @@ public class App {
                 System.out.println("안녕히 가세요!");
                 break;
             }
-            Controller controller=requestHandlerMapping.get(menu);
+            Controller controller=(Controller)iocContainer.getBean(menu);
             
             if (controller !=null) {
                 controller.service(keyIn);
