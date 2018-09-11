13e1719e7b854a32f09515bdb4827d6ba9f51cea
kim <ilos1212@naver.com>
2018-09-11 12:42:22 +0900

    injectDependency()를 별도 클래스로 분리

--- java110-project/src/main/java/bitcamp/java110/cms/context/ApplicationContext.java
+++ java110-project/src/main/java/bitcamp/java110/cms/context/ApplicationContext.java
@@ -15,134 +15,123 @@ import bitcamp.java110.cms.annotation.Autowired;
 import bitcamp.java110.cms.annotation.Component;
 
 public class ApplicationContext {
-    HashMap<String,Object> objPool=new HashMap<>();
-    List<Class<?>> classes=new ArrayList<>();
+    HashMap<String,Object> objPool = new HashMap<>();
+    List<Class<?>> classes = new ArrayList<>();
     
     public ApplicationContext(String packageName) throws Exception {
-        //패키지이름을 파일경로로 바꾼다
-        String path=packageName.replace(".","/");
-        //패키지 경로를 가지고 전체파일 경로를 알아낸다.
-        File file= Resources.getResourceAsFile(path);
-
-        //패키지 폴더에 들어 있는 클래스를 찾아 클래스를 로딩한 후 목록을 리턴한다.
-       findClass(file, path);
         
-       //로딩된 클래스 목록을 뒤져서 @Component가 붙은 
-       //클래스에 대해 인스턴스를 생성하여 objpool에 보관한다
-       createInstance();
+        // 패키지 이름을 파일 경로로 바꾼다.
+        String path = packageName.replace(".", "/");
+        
+        // 패키지 경로를 가지고 전체 파일 경로를 알아낸다.
+        File file = Resources.getResourceAsFile(path);
        
-       //objpool에 보관된 객체를 꺼내 @Autowired가 붙은 셋터를 찾아 호출한다. 
-       //=>의존객체주입
-       injectDependency();
+        // 패키지 폴더에 들어 있는 클래스를 찾아 클래스를 로딩한 후 목록에 저장한다.
+        findClass(file, path);
+        
+        // 로딩된 클래스 목록을 뒤져서 @Component 가 붙은 
+        // 클래스에 대해 인스턴스를 생성하여 objPool에 보관한다.
+        createInstance();
+        
+        // injectDependency() 메서드를 외부 클래스로 분리한 다음에 
+        // 그 객체를 실행한다.
+        AutowiredAnnotationBeanPostProcessor processor = 
+                new AutowiredAnnotationBeanPostProcessor();
+        processor.postProcess(this);
+
     }
-    //objpool에 보관된 객체를 이름으로 찾아 리턴한다
+    
+    // objPool에 보관된 객체를 이름으로 찾아 리턴한다.
     public Object getBean(String name) {
         return objPool.get(name);
-        //objpool에서 주어진 이름의 객체를 찾아 리턴한다
     }
-    //객체의 타입으로 objpool에 보관된 객체를 찾아 리턴한다.
+    
+    // 객체의 타입으로 objPool에 보관된 객체를 찾아 리턴한다.
     public Object getBean(Class<?> type) {
-        Collection<Object> objList=objPool.values();
-        for(Object obj:objList) {
-            if(type.isInstance(obj))
+        Collection<Object> objList = objPool.values();
+        for (Object obj : objList) {
+            if (type.isInstance(obj))
                 return obj;
         }
         return null;
     }
     
     public String[] getBeanDefinitionNames() {
-       Set<String> keySet = objPool.keySet();
-       String[] names =new String[keySet.size()];
-       keySet.toArray(names);
-       return names;
-       
+        Set<String> keySet = objPool.keySet();
+        String[] names = new String[keySet.size()];
+        keySet.toArray(names);
+        return names;
     }
-    private void findClass(File path, String packagePath){
+    
+    private void findClass(File path, String packagePath) {
         File[] files = path.listFiles();
-        for(File file:files) {
-            if(file.isDirectory()) {
-                findClass(file, packagePath+"/"+file.getName());
-            }else {
-                String className=
-                (packagePath+"/"+file.getName()).replace("/",".")
-                        .replace(".class","");
+        for (File file : files) {
+            if (file.isDirectory()) {
+                findClass(file, packagePath + "/" + file.getName());
+            } else {
+                String className = (packagePath + "/" + file.getName())
+                            .replace("/", ".")
+                            .replace(".class", "");
+                
                 try {
-                //1)클래스 이름을 가지고 .class 파일을 찾아 메모리에 로딩한다.
-               Class<?> clazz= Class.forName(className);
-               //=>인터페이스인경우 무시한다.
-               
-               classes.add(clazz);     //로딩한 클래스 정보를 목록에 보관한다    
-                }catch (Exception e) {}
+                    // 클래스 이름을 가지고 .class 파일을 찾아 메모리에 로딩한다.
+                    Class<?> clazz = Class.forName(className);
+                    
+                    classes.add(clazz); // 로딩한 클래스 정보를 목록에 보관한다.
+                } catch (Exception e) {}
+            }
         }
     }
-   }
+    
     private void createInstance() {
-        for (Class<?> clazz:classes) {
-            //=>인터페이스인 경우 무시한다
+        for (Class<?> clazz : classes) {
+            // => 인터페이스인 경우 무시한다.
             if (clazz.isInterface()) continue;
             
-            Component anno=clazz.getAnnotation(Component.class);
-            //@Component 애노테이션이 붙지않은 클래스는 객체를 생성하지 않는다.
-            if(anno==null) continue;
+            // => 클래스에서 Component 애노테이션을 추출한다.
+            Component anno = clazz.getAnnotation(Component.class);
             
+            // => @Component 애노테이션이 붙지 않은 클래스는 객체를 생성하지 않는다.
+            if (anno == null) continue;
             
             try {
-            //2)로딩된 클래스 정보를 가지고 인스턴스를 생성한다.
-            //=>먼저 해당 클래스의 생성자 정보를 얻는다.
-            Constructor<?> consructor= clazz.getConstructor();
-            //생성자 가지고 인스턴스를 생성
-            Object instance= consructor.newInstance();
-            //이름으로 인스턴스의 필드 정보를 찾는다.
-            
-            //=>클래스에서 Component 애노테이션을 추출한다.
-            //System.out.println(clazz.getName()+"==>"+name);
-            
-            //=> Component 애노테이션이 vlaue 값이 있으면 그 값으로 객체를 저장!
-            //없으면 클래스 이름으로 객체를 저장
-            if(anno.value().length() >0) {
-                //Component 애노테이션 value 값으로 인스턴스를 objpool에 저장한다.
-                objPool.put(anno.value(), instance);
-            }else {
-                objPool.put(clazz.getName(),instance);
+                // 로딩된 클래스 정보를 가지고 인스턴스를 생성한다.
+                // => 먼저 해당 클래스의 생성자 정보를 얻는다.
+                Constructor<?> constructor = clazz.getConstructor();
                 
-            }
-            
-            
-            }catch (Exception e) {
+                // => 생성자를 가지고 인스턴스를 생성한다.
+                Object instance = constructor.newInstance();
+                
+                //System.out.println(clazz.getName() + "==> " + name);
+                
+                // => Component 애노테이션이 value 값이 있으면 그 값으로 객체를 저장
+                //    없으면, 클래스 이름으로 객체를 저장한다.
+                if (anno.value().length() > 0) {
+                    // => Component 애노테이션 value 값으로 인스턴스를 objPool에 저장한다.
+                    objPool.put(anno.value(), instance);
+                } else {
+                    objPool.put(clazz.getName(), instance);
+                }
+            } catch (Exception e) {
                 e.printStackTrace();
-                System.out.printf("%s 클래스는 기본 생성자가 없습니다",
+                System.out.printf("%s 클래스는 기본 생성자가 없습니다.", 
                         clazz.getName());
             }
-            
         }
     }
-    private void injectDependency() {
-        //objpool에 보관된 객체 목록을 꺼댄다
-        Collection<Object> objList=objPool.values();
+    
+    /*
+    private void callBeanPostProcessor() {
+        Collection<Object> objList = objPool.values();
         
-        for( Object obj:objList) {
-            //목록에서 객체를 꺼내 @Autowired가 붙은 메서드를 찾는다.
-
-            Method[] methods=obj.getClass().getDeclaredMethods();
-            for (Method m:methods) {
-                if(!m.isAnnotationPresent(Autowired.class)) continue;
-                
-                //setter 메서드의 파라미터 타입을 알아낸다.
-                Class<?> paramType=m.getParameterTypes()[0];
-                
-                //그 파라미터 타입과 일치하는 객체가 objpool에서 꺼낸다.
-                Object dependency = getBean(paramType);
-                
-                if(dependency==null)continue;
-                try {
-                m.invoke(obj, dependency);
-                System.out.printf("%s() 호출됨\n",getName());
-                }catch (Exception e) {}
-            }
+        // => objPool에 보관된 객체 중에서 BeanPostProcessor 규칙을 
+        //    준수하는 객체를 찾는다.
+        for (Object obj : objList) {
+            if (!BeanPostProcessor.class.isInstance(obj)) continue;
+            
+            BeanPostProcessor processor = (BeanPostProcessor)obj;
+            processor.postProcess(this);
         }
     }
-    private Object getName() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    */
 }
---
+++ java110-project/src/main/java/bitcamp/java110/cms/context/BeanPostProcessor.java
@@ -0,0 +1,13 @@
+package bitcamp.java110.cms.context;
+
+import java.util.Map;
+
+//이 규칙은
+//IoC 컨테이너(ApplicationContext)가 객체들을 생성한 후에
+//마무리 작업을 수행하는 객체에 대해 호출하는 규칙이다.
+//따라서 마무리 작업을 수행하는 클래스를 만들떄는
+//반드시 다음 규칙을 준수해야한다.
+public interface BeanPostProcessor {
+    void postProcess(ApplicationContext beanContainer);
+
+}
