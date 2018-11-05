f3c13401851f384081358dd83db02a4f28ec2936
kim <ilos1212@naver.com>
2018-11-05 12:56:51 +0900

    .

--- java110-project/src/main/java/bitcamp/java110/cms/dao/MemberDao.java
+++
@@ -1,8 +0,0 @@
-package bitcamp.java110.cms.dao;
-
-import bitcamp.java110.cms.domain.Member;
-
-public interface MemberDao {
-    int insert(Member member);
-    int delete(int no);
-}
--- java110-project/src/main/java/bitcamp/java110/cms/dao/PhotoDao.java
+++
@@ -1,8 +0,0 @@
-package bitcamp.java110.cms.dao;
-
-import java.util.Map;
-
-public interface PhotoDao {
-    int insert(Map<String,Object> params);
-    int delete(int no);
-}
